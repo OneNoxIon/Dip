6c850ff2041fd91341100f562d101f334dd6c825
Tipa Koder <74534286+OneNoxIon@users.noreply.github.com>
2022-04-30 01:20:18 +0500

    Add Cookies with user info for save login session, and add redirect on profile
    Set_cookies, max_age this is cookie save time in seconds, if cookies exist, redirect on profile

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,5 @@
-from flask import Blueprint, render_template, request
-#from flask_login import login_user, login_required,logout_user, current_user
+from flask import Blueprint, render_template, request, make_response, redirect
 from .models import User
-#from werkzeug import generate_password_hash, check_password_hash      #In progress
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -9,6 +7,8 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/logins', methods=['GET', 'POST'])
 def logins():
+    if request.cookies.get('user'):
+        return redirect('/profile')
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -17,12 +17,15 @@ def logins():
             user = User.query.filter_by(Mailbox=email, Password=password).first()
             if user:
                 print(user.Login + "   loggined")
-                #login_user(user, remember=True)
-                #in progress        if check_password_hash(User.password, password):
-    #data = request.form
-    #print(data)
+                res = make_response("Setting a cookie")
+                res.set_cookie('user', user.Login, max_age=60*60*24)
+                return redirect('/profile')
+
     return render_template('login.html')
     
+@auth.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template('profile.html')
     
     
 @auth.route('/logout')
